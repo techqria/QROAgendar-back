2c4f2496dc3b5e169aac44109eb3763f2ddf155b
Aenã <aenamartinelli@gmail.com>
2022-12-08 23:27:16 -0300

    Roles created (manager, admin, vet)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,25 @@ import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { AdminModule } from './admin/admin.module';
 import { AuthModule } from './auth/auth.module';
+import { ManagerModule } from './manager/manager.module';
 import { UserModule } from './user/user.module';
+import { VetModule } from './vet/vet.module';
 
 @Module({
   imports: [
     AuthModule,
     UserModule,
+    VetModule,
+    AdminModule,
+    ManagerModule,
     ConfigModule.forRoot({ isGlobal: true }),
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       autoSchemaFile: true,
       context: ({ req }) => ({ req })
     }),
-  ],
-  controllers: [],
-  providers: [],
+  ]
 })
 export class AppModule { }
